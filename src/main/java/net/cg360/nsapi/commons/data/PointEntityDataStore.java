62763c7aff239ccc39a1d387aa56864ed70cbd1b
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 15:27:31 +0000

    Added constructor for assembled pointentity

--- src/main/java/net/cg360/nsapi/commons/data/PointEntityDataStore.java
+++ src/main/java/net/cg360/nsapi/commons/data/PointEntityDataStore.java
@@ -53,7 +53,7 @@ public class PointEntityDataStore {
     public static class AssembledPointEntityDataStore extends PointEntityDataStore {
 
         public AssembledPointEntityDataStore(String identifier, String type, PosRot pos, Map<String, String> strings, Map<String, Number> numbers, Map<String, Boolean> switches) {
-            super();
+            super(identifier, type, pos, strings, numbers, switches);
         }
 
     }
