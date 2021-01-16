998342eb90dadff978ffd6ca29fc8a932e386a9a
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 14:15:30 +0000

    Missing property exception

---
+++ src/main/java/net/cg360/nsapi/commons/exception/MissingPropertyException.java
@@ -0,0 +1,8 @@
+package net.cg360.nsapi.commons.exception;
+
+public class MissingPropertyException extends RuntimeException {
+
+    public MissingPropertyException() { super(); }
+    public MissingPropertyException(String str) { super(str); }
+
+}
