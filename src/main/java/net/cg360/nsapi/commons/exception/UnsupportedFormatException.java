33ce5a4e37ce98beeed2954d117f1fb6d755e001
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 18:16:30 +0000

    Added UnsupportedFormatException

---
+++ src/main/java/net/cg360/nsapi/commons/exception/UnsupportedFormatException.java
@@ -0,0 +1,8 @@
+package net.cg360.nsapi.commons.exception;
+
+public class UnsupportedFormatException extends RuntimeException {
+
+    public UnsupportedFormatException() { super(); }
+    public UnsupportedFormatException(String str) { super(str); }
+
+}
