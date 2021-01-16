771a6d9cd92c142401cc0c604250f870d934f3fa
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 12:36:17 +0000

    Added random string gen to Utility.

--- src/main/java/net/cg360/nsapi/commons/Utility.java
+++ src/main/java/net/cg360/nsapi/commons/Utility.java
@@ -1,5 +1,6 @@
 package net.cg360.nsapi.commons;
 
+import java.util.Random;
 import java.util.regex.Pattern;
 
 /**
@@ -7,6 +8,8 @@ import java.util.regex.Pattern;
  * @author CG360
  */
 public class Utility {
+
+    public static final char[] UNIQUE_TOKEN_CHARACTERS = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','$','&','*','^','%','(',')'};
 
     public static String createNamespacedID(String identifier){
         return createNamespacedID(CommonConstants.COMMON_PREFIX, identifier);
@@ -18,6 +21,16 @@ public class Utility {
         } else {
             return identifier;
         }
+    }
+
+    public static String generateUniqueToken(int minlength, int variation){
+        int length = minlength + (variation > 0 ? new Random().nextInt(variation) : 0);
+        String fstr = "";
+        for(int i = 0; i < length; i++){
+            Random r = new Random();
+            fstr = fstr.concat(String.valueOf(UNIQUE_TOKEN_CHARACTERS[r.nextInt(UNIQUE_TOKEN_CHARACTERS.length)]));
+        }
+        return fstr;
     }
 
 }
