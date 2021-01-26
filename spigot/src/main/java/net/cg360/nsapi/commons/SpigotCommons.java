d46ed4a281686e0e6694799b4b4d7a1aa53cb283
Will <willscullythewag@gmail.com>
2021-01-26 14:18:06 +0000

    Updated plugin.yml

--- spigot/src/main/java/net/cg360/nsapi/commons/SpigotCommons.java
+++ spigot/src/main/java/net/cg360/nsapi/commons/SpigotCommons.java
@@ -2,6 +2,10 @@ package net.cg360.nsapi.commons;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Spigot plugin for loading the Commons API on a spigot server.
+ * It also may include spigot-specific common methods.
+ */
 public class SpigotCommons extends JavaPlugin {
 
     public static SpigotCommons plg;
