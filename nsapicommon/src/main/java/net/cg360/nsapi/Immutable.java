d9c01b0045cd0e632cbfac0248bc2ce5d454ff51
Will <willscullythewag@gmail.com>
2021-02-09 22:46:19 +0000

    Added ByteBuffer immutable method.

--- nsapicommon/src/main/java/net/cg360/nsapi/Immutable.java
+++ nsapicommon/src/main/java/net/cg360/nsapi/Immutable.java
@@ -1,5 +1,6 @@
 package net.cg360.nsapi;
 
+import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,10 @@ public class Immutable {
 
     public static <V> List<V> uList(List<V> obj, boolean isUnmodifiable){
         return isUnmodifiable ? Collections.unmodifiableList(obj) : obj;
+    }
+
+    public static ByteBuffer uBuffer(ByteBuffer obj, boolean isUnmodifiable){
+        return isUnmodifiable ? ((ByteBuffer) obj.clear()).asReadOnlyBuffer() : obj;
     }
 
 }
