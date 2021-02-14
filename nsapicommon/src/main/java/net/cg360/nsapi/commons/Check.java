1cc201235a53a9c95990b9aae82bf847de7dbe1b
Will <willscullythewag@gmail.com>
2021-02-14 11:02:21 +0000

    Added Check utility class

---
+++ nsapicommon/src/main/java/net/cg360/nsapi/commons/Check.java
@@ -0,0 +1,19 @@
+package net.cg360.nsapi.commons;
+
+import net.cg360.nsapi.commons.exception.MissingPropertyException;
+
+public final class Check {
+
+    public static void missingProperty(Object obj, String loc, String name) {
+        if(isNonNull(obj)) throw new MissingPropertyException(String.format("%s is missing a '%s' property.", loc, name));
+    }
+
+    public static void nullParam(Object obj, String loc, String name) {
+        if(isNonNull(obj)) throw new IllegalArgumentException(String.format("'%s' in %s cannot be null.", loc, name));
+    }
+
+    public static boolean isNonNull(Object obj) {
+        return obj != null;
+    }
+
+}
