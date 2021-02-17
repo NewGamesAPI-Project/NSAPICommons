dec7c5151e4d169dc9f199114c33d673eca875cb
Will <willscullythewag@gmail.com>
2021-02-17 20:49:03 +0000

    Add Static Player Whitelist filter (Spigot)

---
+++ spigot/src/main/java/net/cg360/nsapi/commons/event/filter/FilterStaticPlayerWhitelist.java
@@ -0,0 +1,60 @@
+package net.cg360.nsapi.commons.event.filter;
+
+import net.cg360.nsapi.commons.event.Event;
+import net.cg360.nsapi.commons.event.type.EntityEvent;
+import net.cg360.nsapi.commons.event.type.PlayerEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+/**
+ * Taking in a whitelist, the filter checks if the event passed in
+ * has any of the players on the whitelist mentioned within it. If it does,
+ * it returns a true.
+ */
+public class FilterStaticPlayerWhitelist implements EventFilter {
+
+    protected Player[] playerWhitelist;
+    protected boolean onlyPlayerEvents;
+
+    public FilterStaticPlayerWhitelist(Player[] playerWhitelist, boolean onlyPlayerEvents) {
+        this.playerWhitelist = playerWhitelist;
+        this.onlyPlayerEvents = onlyPlayerEvents;
+    }
+
+    @Override
+    public boolean checkEvent(Event eventIn) {
+        boolean pass = true;
+        // Should be true if event is not the right type.
+        // Should be false if it's the right type but failed all checks.
+
+        if(eventIn instanceof PlayerEvent) {
+            pass = false; // A check has been done, make false unless found.
+            Player check = ((PlayerEvent) eventIn).getPlayer();
+
+            for(Player p: playerWhitelist) {
+
+                if(p == check) {
+                    return true; // Passed thus it's guaranteed true
+                }
+            }
+        }
+
+        if((!onlyPlayerEvents) && (eventIn instanceof EntityEvent)) {
+            pass = false; // A check has been done, make false unless found.
+            Entity check = ((EntityEvent) eventIn).getEntity();
+
+            if(check instanceof Player) {
+
+                for (Player p : playerWhitelist) {
+
+                    if (p == check) {
+                        pass = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return pass;
+    }
+}
