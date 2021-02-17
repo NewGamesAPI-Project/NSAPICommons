6b33be7551a32b7f1d8199e21340dae2aa2fb60a
Will <willscullythewag@gmail.com>
2021-02-17 20:48:36 +0000

    Add PlayerEvent + EntityEvent event types (Spigot)

---
+++ spigot/src/main/java/net/cg360/nsapi/commons/event/type/EntityEvent.java
@@ -0,0 +1,9 @@
+package net.cg360.nsapi.commons.event.type;
+
+import org.bukkit.entity.Entity;
+
+public interface EntityEvent {
+
+    Entity getEntity();
+
+}
---
+++ spigot/src/main/java/net/cg360/nsapi/commons/event/type/PlayerEvent.java
@@ -0,0 +1,9 @@
+package net.cg360.nsapi.commons.event.type;
+
+import org.bukkit.entity.Player;
+
+public interface PlayerEvent {
+
+    Player getPlayer();
+
+}
